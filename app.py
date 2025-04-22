2abf697a180191390b8f456150b0d3eadcf16799
Jason Burns <MettleFitness@Jasons-MacBook-Air.local>
2025-04-22 15:50:10 -0500

    Fix Streamlit path resolution with sys.path.append

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import streamlit as st
 
 from nba.logic import run_nba_pick6
 from mlb.logic import run_mlb_pick6
 from dfs.logic import run_dfs
+
 
 
 def detect_sport_from_files(files):
